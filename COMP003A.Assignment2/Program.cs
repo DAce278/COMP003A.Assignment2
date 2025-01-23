2429577eb930a084faf5373a281efbff14540c42
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2025-01-22 20:11:09 -0800

    Added section that asks for user information.

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -2,15 +2,71 @@
  * Author: Deegan Acevedo
  * Course: COMP003A
  * Faculty: Jonathan Cruz
- * Purpose
+ * Purpose:
  */
 namespace COMP003A.Assignment2
 {
     internal class Program
     {
         static void Main(string[] args)
+            
         {
-            Console.WriteLine("Hello, World!");
+            // Introduce variables
+            string firstName;
+            string lastName;
+            int age;
+            double price;
+            bool isStudent;
+
+
+            // Introductory title
+            Console.WriteLine("*************************************\n" +
+                                "Welcome to the Discount Calculator!\n" +
+								"*************************************");
+
+            // asks user for first name
+            Console.Write("What is your first name? ");
+            firstName = Console.ReadLine();
+
+            // asks user for last name
+            Console.Write("What is your last name? ");
+            lastName = Console.ReadLine();
+
+            // asks user for age
+            Console.Write("What is your age? ");
+            age = int.Parse(Console.ReadLine());
+
+            // asks user for product price
+            Console.Write("What is the price of the item you're interested in? ");
+            price = double.Parse(Console.ReadLine());
+
+            // asks user if they are a student
+            Console.Write("Are you a student? (true/false): ");
+            isStudent = bool.Parse(Console.ReadLine());
+
+
+            // conversion of values
+            int newAge;
+            newAge = age + 5;
+
+            double studentPrice;
+
+
+           
+
+
+            Console.WriteLine($"\n\n\n*************************************\n" +
+                $"Hello {firstName} {lastName}!\n" + // Welcomes user by name
+                $"Your current age is {age}\n" + // Outputs users age
+                $"In 5 years, you will be {newAge}\n" + // Outputs users age in 5 years
+                $"The original price of the item is $"); 
+
+
+            
+
+
+           
+
         }
     }
 }
