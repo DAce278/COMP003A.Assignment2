9685aa5153341d87d23ed2b84a4a52a98cb19bf4
deeganacevedo <deeganacevedo@LEM231-08>
2025-01-22 18:56:36 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Author: Deegan Acevedo
+ * Course: COMP003A
+ * Faculty: Jonathan Cruz
+ * Purpose
+ */
+namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
